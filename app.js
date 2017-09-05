587846b779bfa90953fa782df0bd6bbe1377714a
Nik <nickscorpio4u@gmail.com>
2017-09-05 16:57:08 +0000

    first commit

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 var express = require("express"),
 bodyParser  = require("body-parser"),
-mongoose    = require("mongoose"),
+// mongoose    = require("mongoose"),
 app         = express();
 
 
 // APP/ Mongoose configs
 
-mongoose.connect("mongodb://localhost/restful_blog_app");
+// mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 
-var blogSchema = mongoose.Schema({
-   title : String,
-   image : String,
-   body : String,
-   created : {
-                type : Date, 
-                default : Date.now()
-            }
-});
+// var blogSchema = mongoose.Schema({
+//   title : String,
+//   image : String,
+//   body : String,
+//   created : {
+//                 type : Date, 
+//                 default : Date.now()
+//             }
+// });
 
 // Mongoose MODEL
-var Blog = mongoose.model("Blog",blogSchema);
+// var Blog = mongoose.model("Blog",blogSchema);
 
 // Blog.create({
     
@@ -38,16 +38,18 @@ app.get("/",function(req,res){
 
 //RESTFul Routes
 app.get("/blogs",function(req,res){
-    Blog.find({},function(err,blogs){
-       if(err){
-           console.log("error fetching data");
-       }
-       else
-       {
-            res.render("index", {blogs:blogs});         
-       }
+    // Blog.find({},function(err,blogs){
+    //   if(err){
+    //       console.log("error fetching data");
+    //   }
+    //   else
+    //   {
+    //         res.render("index", {blogs:blogs});         
+    //   }
         
-    });
+    // });
+    
+    res.render("index"); 
    
 });
 
